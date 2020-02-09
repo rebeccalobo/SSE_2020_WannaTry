bc8bb0ef12985160c1d077017db9397b0e24d21b
rebeccalobo <rebecca.lobo@ucdconnect.ie>
2020-02-09 21:56:26 +0000

    foundations of database started - mySQL attached to Intellij. "students" table needs to be correctly assigned the MySQL @localhost data source
    Signed-off-by: rebeccalobo <rebecca.lobo@ucdconnect.ie>

---
+++ src/main/java/exceptions/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class StudentNotFoundException extends Exception {
+    private Long student_id;
+    public StudentNotFoundException(Long student_id){
+        super(String.format("No student was found with id: %s", student_id));
+    }
+}
