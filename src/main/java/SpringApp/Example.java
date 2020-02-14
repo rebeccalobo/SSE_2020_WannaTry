2b0cbe58f01b16a3dc37d001562acea791e01b8d
rebeccalobo <rebecca.lobo@ucdconnect.ie>
2020-02-14 10:48:40 +0000

    small database push
    Signed-off-by: rebeccalobo <rebecca.lobo@ucdconnect.ie>

--- src/main/java/SpringApp/Example.java
+++ src/main/java/SpringApp/Example.java
@@ -1,15 +1,14 @@
 package SpringApp;
 
+import SpringApp.controller.HomePageController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 
-//@SpringBootApplication(scanBasePackages = {"com.SSE2020.WannaTry"})
-@SpringBootApplication
-@ComponentScan("com")
+@SpringBootApplication(scanBasePackageClasses = HomePageController.class)
 public class Example extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(Example.class, args);
     }
